ff23d2c57618ed4001864cb5a1abbbc1963c1beb
Almuheetu shihab <almuheetu@gmail.com>
2024-08-29 14:57:46 +0600

    first stape full complete

---
+++ app/src/main/java/com/example/navigation/SecondFragment.kt
@@ -0,0 +1,36 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.navigation.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+         binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+
+    }
+
+}
